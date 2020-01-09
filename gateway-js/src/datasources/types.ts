8bda6cd1b443722e4bb130071acbde5c9d9e9c9d
vladislav-kiva <vladislav.kiva@moneyman.ru>
2020-01-09 17:18:52 +0300

    feat(apollo-gateway): opentracing

--- gateway-js/src/datasources/types.ts
+++ gateway-js/src/datasources/types.ts
@@ -1,7 +1,9 @@
 import { GraphQLResponse, GraphQLRequestContext } from 'apollo-server-types';
+import { Headers } from 'apollo-server-env';
 
 export interface GraphQLDataSource<TContext extends Record<string, any> = Record<string, any>> {
   process(
     request: Pick<GraphQLRequestContext<TContext>, 'request' | 'context'>,
+    headers?: Headers
   ): Promise<GraphQLResponse>;
 }

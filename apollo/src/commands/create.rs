640c92bc6f62eb0071c7dddda8a80fab161ca07a
Joshua Segaran <joshua.segaran@apollographql.com>
2020-04-16 19:23:29 -0400

    More stufz

--- apollo/src/commands/create.rs
+++ apollo/src/commands/create.rs
@@ -9,14 +9,30 @@ impl Command for CreateGraph {
             Err(e) => {
                 println!("Error authenticating: {}", e);
                 return;
-            },
+            }
             Ok(token) => token,
         };
         let gql_client = graphql::client::ApolloCloudClient::new(
             String::from("https://engine-staging-graphql.apollographql.com"),
-            auth_token
+            auth_token,
         );
-        gql_client.get_org_memberships();
+
+        let accounts = gql_client.get_org_memberships();
+        let accounts_pretty = format!("{:?}", accounts);
+        let account = if (accounts.is_empty()) {
+            println!("You are not a member of any organization");
+            return;
+        } else if (accounts.len() > 1) {
+            let prompt_string = format!("Please choose an organization to own the graph: Options {}", accounts_pretty);
+            let chosen_account = utils::get_user_input(&prompt_string).unwrap();
+            if !accounts.contains(&chosen_account) {
+                println!("Chose a bad account");
+                return;
+            }
+            chosen_account
+        } else {
+            String::from(accounts.iter().next().unwrap())
+        };
         let graph_id = utils::get_user_input("Choose a name for your graph (cannot be changed)").unwrap();
         println!("You have chosen {}. Excellent selection.", graph_id);
     }
--- apollo/src/graphql/client.rs
+++ apollo/src/graphql/client.rs
@@ -1,11 +1,15 @@
 use serde_json::{Result, Value, Map};
-use serde::Serialize;
-use std::collections::HashMap;
+use reqwest::blocking::{Client, ClientBuilder};
+use serde::{Serialize, Deserialize};
+use std::collections::{HashMap, HashSet};
+use reqwest::header::{HeaderMap, HeaderValue};
+use std::vec::Vec;
+use std::iter::FromIterator;
 
 pub struct ApolloCloudClient {
     endpoint_url: String,
     auth_token: String,
-    client: reqwest::blocking::Client,
+    client: Client,
 }
 
 #[derive(Serialize)]
@@ -13,9 +17,34 @@ struct Query {
     query: String,
 }
 
+#[derive(Deserialize)]
+struct GetOrgMembershipResponse__account {
+    id: String
+}
+
+#[derive(Deserialize)]
+struct GetOrgMembershipResponse__membership {
+   account: GetOrgMembershipResponse__account
+}
+
+#[derive(Deserialize)]
+struct GetOrgMembershipRespose__memberships {
+  memberships: std::vec::Vec<GetOrgMembershipResponse__membership>
+}
+
+#[derive(Deserialize)]
+struct GetOrgMembershipResponse__me {
+   me: GetOrgMembershipRespose__memberships
+}
+
+#[derive(Deserialize)]
+struct GetOrgMembershipResponse {
+    data: GetOrgMembershipResponse__me
+}
+
 impl ApolloCloudClient {
     pub fn new(endpoint_url: String, auth_token: String) -> ApolloCloudClient {
-        let client = reqwest::blocking::Client::new();
+        let client = Client::new();
         ApolloCloudClient {
             endpoint_url,
             auth_token,
@@ -23,12 +52,21 @@ impl ApolloCloudClient {
         }
     }
 
-    pub fn get_org_memberships(&self) {
+    pub fn get_org_memberships(&self) -> HashSet<String> {
         let mut operation_map = HashMap::new();
         operation_map.insert("query", GET_ORG_MEMBERSHIPS_QUERY);
-        let res = self.client.post("https://engine-graphql.apollographql.com/api/graphql")
-            .json::<HashMap<&str,&str>>(&operation_map).send();
-        println!("{:#?}", res);
+        let mut headers = HeaderMap::new();
+        headers.insert("X-API-KEY",
+                       HeaderValue::from_str(self.auth_token[..].as_ref()).unwrap());
+        let res = match self.client.post("https://engine-staging-graphql.apollographql.com/api/graphql")
+            .headers(headers)
+            .json::<HashMap<&str, &str>>(&operation_map).send() {
+            Ok(res) => res,
+            Err(e) => panic!(e)
+        };
+        let text = res.text().unwrap();
+        let results = serde_json::from_str::<GetOrgMembershipResponse>(&text).unwrap();
+        HashSet::from_iter(results.data.me.memberships.into_iter().map(|it| it.account.id).collect::<Vec<String>>())
     }
 }
 
